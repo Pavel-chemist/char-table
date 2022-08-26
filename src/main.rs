f69d36efd629817b71e89bc45de63b56d44cc58e
Pavel-chemist <pavel.chemist@gmail.com>
2022-08-26 19:52:50 +0400

    start

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    // let a: u8 = 100;
+
+    for i in 0..255 {
+        println!("Char with code {} is {}", i, i as u8 as char);
+    }
+    
 }
